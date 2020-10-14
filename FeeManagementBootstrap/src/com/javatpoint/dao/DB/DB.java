bdc1e3af3a5b1a5ba8f63d0abc393214a3900abc
Somi <Somi@LAPTOP-TF7V4FGH>
2020-10-14 16:10:03 +0530

    Initial Commit

---
+++ FeeManagementBootstrap/src/com/javatpoint/dao/DB/DB.java
@@ -0,0 +1,12 @@
+package com.javatpoint.dao.DB;
+import java.sql.*;
+public class DB {
+public static Connection getCon(){
+	Connection con=null;
+	try{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","scott","tiger");
+	}catch(Exception ex){System.out.println(ex);}
+	return con;
+}
+}
